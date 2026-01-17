19d7ee14a88b45010ce0f9e7ed64201342846f32
mnove <novelli3000@gmail.com>
2026-01-17 16:58:40 +0100

    feat: ad og and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,30 @@
 import { RootProvider } from "fumadocs-ui/provider/next";
 import "./global.css";
 import { Inter } from "next/font/google";
+import { Metadata } from "next";
 
 const inter = Inter({
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s | carouselcn",
+    default: "carouselcn",
+  },
+  description: "Copy-paste carousel components for React.",
+  keywords: [
+    "nextjs",
+    "next",
+    "carousel",
+    "carousel component",
+    "carouselcn",
+    "typescript",
+    "react",
+    "react carousel",
+  ],
+  metadataBase: new URL("https://carouselcn.vercel.app"),
+};
 
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
